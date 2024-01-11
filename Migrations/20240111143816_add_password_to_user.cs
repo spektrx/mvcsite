e6e01a1c6f429325882e899d3d83e94a4518fc16
box40404 <pashayashik@mail.ru>
2024-01-11 17:41:34 +0300

    edit db

---
+++ Migrations/20240111143816_add_password_to_user.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvcsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_password_to_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Balance { get; set; }
+        public string Password { get; set; }
+        public double Balance { get; set; } = 0;
     }
 }
