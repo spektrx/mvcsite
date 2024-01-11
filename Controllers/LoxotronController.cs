d2b061e9fefb935a2c7092972d109129cbd443ee
box40404 <pashayashik@mail.ru>
2024-01-11 19:28:56 +0300

    да мне по...

--- Controllers/LoxotronController.cs
+++ Controllers/LoxotronController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+
 using System.Text.Encodings.Web;
 
 namespace Loxotron.Controllers;
@@ -14,10 +15,71 @@ public class LoxotronController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Loxotron(int first, int second)
+    public IActionResult Play(int first, float second)
     {
-        int result = first * second;
-        return $"{result}";
+
+        Random random = new Random();
+        int coins = 10000;  //θη αδ
+        float[] story = new float[10];
+
+
+
+        int win = 0;
+        Console.WriteLine(string.Join(", ", story));
+        Console.WriteLine("Your real money: " + coins);
+        Console.Write("Bet: ");
+        int bet = first;
+        Console.Write("Choose the coefficient: ");
+        float a = second;
+
+
+        if (bet > coins)
+        {
+            return View();
+        }
+
+        coins -= bet;
+        float cef = 1;
+        int crush = 0;
+        int ran = random.Next(20);
+        float freez = 2;
+        int count = 0;
+
+        while (crush == 0)
+        {
+            Console.WriteLine(Math.Round(cef, 4));
+            //System.Threading.Thread.Sleep((int)(freez * 1000));
+
+            if (ran == 0)
+            {
+                crush = 1;
+
+                if (a <= cef)
+                {
+                    win = 1;
+                }
+
+                break;
+            }
+
+            cef *= 1.01f;
+            freez *= 0.9f;
+            count += 1;
+            
+            ran = random.Next(50);
+        }
+
+        if (win == 1)
+        {
+            coins += (int)(bet * a);
+        }
+
+        Array.Copy(story, 0, story, 1, 9);
+        story[0] = (float)Math.Round(cef, 4);
+
+        ViewData["Data"] = new { cef = cef, Count = count};
+        return View();
     }
     //http://localhost:5272/helloworld/welcome?first=18&second=34
 }
+
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using mvcsite.Data;
+using mvcsite.Models;
+using System.Security.Claims;
+
+namespace mvcsite.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        ApplicationContext db;
+
+        public RegisterController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<RedirectResult> Register(IFormCollection form)
+        {
+            string name = form["name"];
+            string password = form["password"];
+
+            User user = new() {Name=name, Password=password };
+
+            await db.Users.AddAsync(user);
+
+            await db.SaveChangesAsync();
+
+            List<Claim> claims = new List<Claim>() { new Claim(ClaimTypes.Name, name) };
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+            await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(claimsIdentity));
+
+            return Redirect("/");
+        }
+    }
+}
